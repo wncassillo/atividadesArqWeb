65736f106c474f1b18fdff8b40ff35a82d6446a2
wncassillo <cassilloneto@gmail.com>
2024-03-26 17:56:30 -0300

    atividade aula2

---
+++ src/main/java/arquiteturaweb/ac1/wilsoncn/WilsoncnApplication.java
@@ -0,0 +1,13 @@
+package arquiteturaweb.ac1.wilsoncn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilsoncnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WilsoncnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arquiteturaweb/ac1/wilsoncn/WilsoncnApplicationTests.java
@@ -0,0 +1,13 @@
+package arquiteturaweb.ac1.wilsoncn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilsoncnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
